05db6e2c199981fb82185398bb1561cfe66eff33
januszekm3 <jmirosla@student.agh.edu.pl>
2014-11-30 19:13:45 +0100

    Test

--- src/Test.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Janusz on 30-11-2014.
- */
-public class Test {
-    
-}
---
+++ src/to2/dice/server/Server.java
@@ -0,0 +1,7 @@
+package to2.dice.server;
+
+/**
+ * Created by Janusz on 30-11-2014.
+ */
+public class Server {
+}
