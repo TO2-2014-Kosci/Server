1730e1b42e749f1377f2628b1a08460bdd5c0dce
januszekm3 <jmirosla@student.agh.edu.pl>
2014-11-30 18:24:23 +0100

    change Set to Hashtable

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Janusz on 10-11-2014.
- */
-public class Main {
-    public static void main(String[] args) {
-        GujlebGuj guj = new GujlebGuj();
-        guj.run();
-    }
-}
--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,12 @@
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by Janusz on 10-11-2014.
  */
+
 public class Server {
     Hashtable<Player, GameController> players = new Hashtable<Player, GameController>();
     Set<GameController> controllers = new HashSet<GameController>();
@@ -15,25 +17,52 @@ public class Server {
     public void run() {
     }
 
-    public boolean createRoom(String pokerid, String game, String message) {
-        GameController newGameController = new GameController(pokerid, game, message);
-        controllers.add(newGameController);
-        newGameController.run();
-        return true;
+    public Response login (String login) {
+        Player player = new Player();
+        player.setLogin(login);
+
+        if (players.contains(player)) {
+            return new Response(Response.Type.FAILURE);
+        } else {
+            players.put(player, null);
+            return new Response(Response.Type.SUCCESS);
+        }
     }
 
-    public boolean addPlayerToPoker(String pokerid, Player player) {
-        for (GameController p : controllers)
-            if (p.getPokerID() == pokerid) {
-                if (players.put(player, p)==null) p.addPlayer(player);
-                return true;
-            }
-        return false;
+    public Response createRoom(String roomName, GameSettings roomSettings, String login) {
+        GameController gameController = createGameControler(this, roomSettings); //???
+        for (GameController c : controllers)
+            if (c.roomName.equals(gameController.roomName)) return new Response(Response.Type.FAILURE);
+        controllers.add(gameController);
+        return new Response(Response.Type.SUCCESS);
     }
 
-    public void displayPlayersTable(String pokername){
-        for (GameController p : controllers)
-            if (p.getPokerID() == pokername)
-                p.displayListOfplayers();
+    public Response handleGameAction (GameActionType action) {
+        Player player = getPlayer(action.getLogin(), false);
+        if (player == null) return new Response(ResponseType.FAILURE, "Not logged in");
+        GameController gameController = controllers.get(plyer);
+        Response response = new Response(gameController.handleGameAction(action));
+        return response;
+    }
+
+    public List<GameInfo> getRoomList() {
+        List<GameInfo> roomList = new List<GameInfo>();
+        for (GameController c : controllers) {
+            GameInfo gameInfo = c.getGameInfo;
+            roomList.add(gameInfo);
+        }
+        return roomList;
+    }
+
+    private Player getPlayer(String login, boolean create) {
+        for (Player c : players)
+            if (c.getLogin == login) return c;
+        if (create) {
+            Player player = new Player();
+            player.setLogin(login);
+            return player;
+        } else {
+            return null;
+        }
     }
 }
