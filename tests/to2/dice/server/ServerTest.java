f8c647f4922e84817b981e1705941fcbaf38b32b
januszekm3 <jmirosla@student.agh.edu.pl>
2014-12-01 22:04:37 +0100

    create testCreateRoom()

--- tests/to2/dice/server/ServerTest.java
+++ tests/to2/dice/server/ServerTest.java
@@ -1,6 +1,8 @@
 package to2.dice.server;
 
 import org.junit.Test;
+import to2.dice.game.GameSettings;
+import to2.dice.game.GameType;
 import to2.dice.messaging.Response;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -14,17 +16,30 @@ public class ServerTest {
 
     @Test
     public void testLogin() throws Exception {
-        Response r1, r2, r3, r4;
+        Response [] responses = new Response[4];
 
-        r1 = server.login(null);
-        r2 = server.login("Janusz");
-        r3 = server.login("Januszek");
-        r4 = server.login("Janusz");
+        responses[0] = server.login(null);
+        responses[1] = server.login("Janusz");
+        responses[2] = server.login("Januszek");
+        responses[3] = server.login("Janusz");
 
-        assertEquals("Response r1 = server.login(null) has type SUCCESS", r1.type, Response.Type.SUCCESS);
-        assertEquals("Response r2 = server.login(\"Janusz\") has type SUCCESS", r2.type, Response.Type.SUCCESS);
-        assertEquals("Response r3 = server.login(\"Januszek\") has type SUCCESS", r3.type, Response.Type.SUCCESS);
-        assertEquals("Response r4 = server.login(\"Janusz\") has type FAILURE", r4.type, Response.Type.FAILURE);
+        assertEquals("Response r1 = server.login(null) has type SUCCESS", responses[0].type, Response.Type.SUCCESS);
+        assertEquals("Response r2 = server.login(\"Janusz\") has type SUCCESS", responses[1].type, Response.Type.SUCCESS);
+        assertEquals("Response r3 = server.login(\"Januszek\") has type SUCCESS", responses[2].type, Response.Type.SUCCESS);
+        assertEquals("Response r4 = server.login(\"Janusz\") has type FAILURE", responses[3].type, Response.Type.FAILURE);
+    }
 
+    @Test
+    public void testCreateRoom() throws Exception {
+        String [] roomNames = new String [3];
+        roomNames[0] = "Sosnowiec";
+        roomNames[1] = "Gwozdziec";
+        roomNames[2] = "Sosnowiec";
+
+        GameSettings roomSettings = mock (GameSettings.class);
+
+        System.out.println(roomSettings.getGameType());
+
+        String creator = "Alfons";
     }
 }
