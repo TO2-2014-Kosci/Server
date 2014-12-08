919acc94c936126618dbb9d06ac8f180b1b250cb
januszekm3 <jmirosla@student.agh.edu.pl>
2014-12-08 18:58:51 +0100

    create BotsGameTest

---
+++ tests/to2/dice/server/BotsGameTest.java
@@ -0,0 +1,19 @@
+package to2.dice.server;
+
+import to2.dice.game.BotLevel;
+import to2.dice.game.GameSettings;
+import to2.dice.game.GameType;
+
+import java.util.HashMap;
+
+public class BotsGameTest {
+    HashMap<BotLevel, Integer> botsNumber;
+
+    public void run() {
+        botsNumber.put(BotLevel.HIGH, 0);
+        botsNumber.put(BotLevel.HIGH, 1);
+        botsNumber.put(BotLevel.LOW, 2);
+        botsNumber.put(BotLevel.LOW, 3);
+        GameSettings gameSettings = new GameSettings(GameType.POKER, 5, "Slupsk", 1, 20, 3, 3, botsNumber);
+    }
+}
