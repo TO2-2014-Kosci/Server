59031565617d50d0789bb40f3c632fd1b6207c46
januszekm3 <jmirosla@student.agh.edu.pl>
2014-12-08 22:39:27 +0100

    create NullPointerException

--- tests/to2/dice/server/BotsGameTest.java
+++ tests/to2/dice/server/BotsGameTest.java
@@ -1,9 +1,11 @@
 package to2.dice.server;
 
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import to2.dice.game.BotLevel;
 import to2.dice.game.GameSettings;
 import to2.dice.game.GameType;
+import to2.dice.messaging.Response;
 
 import java.util.HashMap;
 
@@ -13,12 +15,20 @@ import java.util.HashMap;
 public class BotsGameTest {
     HashMap<BotLevel, Integer> botsNumber;
 
+    @InjectMocks
+    Server server;
+
     @Test
-    public void run() {
+    public void run() throws Exception {
+        botsNumber = new HashMap<BotLevel, Integer>();
+
         botsNumber.put(BotLevel.HIGH, 0);
         botsNumber.put(BotLevel.HIGH, 1);
         botsNumber.put(BotLevel.LOW, 2);
         botsNumber.put(BotLevel.LOW, 3);
         GameSettings gameSettings = new GameSettings(GameType.POKER, 5, "Slupsk", 1, 20, 3, 3, botsNumber);
+
+        Response responseLogin = server.login("Ambrozy"); // <-NullPointerException
+//drugiego nie pokaze :p
     }
 }
