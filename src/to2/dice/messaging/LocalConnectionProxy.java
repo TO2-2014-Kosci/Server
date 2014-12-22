37ba698a89d8b43ad7a54330e51c89449fca8be7
fanfilmu <fanflm@gmail.com>
2014-12-22 22:48:55 +0100

    Fix handling exceptions

--- src/to2/dice/messaging/LocalConnectionProxy.java
+++ src/to2/dice/messaging/LocalConnectionProxy.java
@@ -42,7 +42,7 @@ public class LocalConnectionProxy extends AbstractConnectionProxy {
         try {
             return super.joinRoom(roomName);
         }
-        catch(Exception e) {
+        catch(TimeoutException e) {
             return new Response(Response.Type.FAILURE, "Unknown error");
         }
     }
@@ -52,7 +52,7 @@ public class LocalConnectionProxy extends AbstractConnectionProxy {
         try {
             return super.leaveRoom();
         }
-        catch(Exception e) {
+        catch(TimeoutException e) {
             return new Response(Response.Type.FAILURE, "Unknown error");
         }
     }
@@ -62,7 +62,7 @@ public class LocalConnectionProxy extends AbstractConnectionProxy {
         try {
             return super.sitDown();
         }
-        catch(Exception e) {
+        catch(TimeoutException e) {
             return new Response(Response.Type.FAILURE, "Unknown error");
         }
     }
@@ -72,7 +72,7 @@ public class LocalConnectionProxy extends AbstractConnectionProxy {
         try {
             return super.standUp();
         }
-        catch(Exception e) {
+        catch(TimeoutException e) {
             return new Response(Response.Type.FAILURE, "Unknown error");
         }
     }
@@ -82,7 +82,7 @@ public class LocalConnectionProxy extends AbstractConnectionProxy {
         try {
             return super.reroll(dice);
         }
-        catch(Exception e) {
+        catch(TimeoutException e) {
             return new Response(Response.Type.FAILURE, "Unknown error");
         }
     }
